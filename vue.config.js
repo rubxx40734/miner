36c2a50fb9f400c974cf2317e7894f2475e5aed1
bill60800 <rubxx40734@gmail.com>
2022-07-19 17:40:24 +0800

    ver5.0

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicPath: '/miner/dist',
+    // 這邊是更改網頁標題的小圖案
+    pwa: {
+      iconPaths: {
+        favicon32: './favicon.ico',
+        favicon16: './favicon.ico',
+        appleTouchIcon: './favicon.ico',
+        maskIcon: './favicon.ico',
+        msTileImage: './favicon.ico'
+      }
+    }
+  }
